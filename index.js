b20f11849522f799abc77a89762488ac9889de8b
Michael Fine <mfine15@gmail.com>
2014-09-29 23:00:58 -0400

    fixed indentation

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ module.exports = {
 	},
 	makeQueries: function(dir){
 
-		var queries = {};
+    var queries = {};
 
     //cache which are directories to prevent unneeded reads
     var dirs = {};
@@ -31,7 +31,7 @@ module.exports = {
     //to avoid lexical scoping issues
     var that = this;
 
-		var files = fs.readdirSync(dir).filter(function(file){
+    var files = fs.readdirSync(dir).filter(function(file){
       if(isDir(path.join(dir,file))){
         dirs[file] = true;
         return true
@@ -40,20 +40,23 @@ module.exports = {
       return isSql(file);
     });
 
-		var contents = files.map(function(fileName){
+    var contents = files.map(function(fileName){
       if(dirs[fileName]){
         return that.makeQueries(path.join(dir,fileName));
       }
-			return fs.readFileSync(path.join(dir,fileName)).toString();
-		});
 
-		files.forEach(function(fileName,i){
+      return fs.readFileSync(path.join(dir,fileName)).toString();
+    });
+
+    files.forEach(function(fileName,i){
       if(dirs[fileName]){
         queries[fileName] = contents[i];
       }
-			queries[fileName.slice(0,-4)] = makeFunction(contents[i]);
-		});
-		return queries;
-	}
+
+      queries[fileName.slice(0,-4)] = makeFunction(contents[i]);
+    });
+
+    return queries;
+  }
 };
 
